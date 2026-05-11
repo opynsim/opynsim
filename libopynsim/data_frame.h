28cbdb3a37faa3aadd1307258ea1369467c30191
Adam Kewley <contact@adamkewley.com>
2026-05-11 13:54:26 +0200

    Start implementing opyn::DataFrame support for STO files

---
+++ libopynsim/data_frame.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace opyn
+{
+    /// Represents a two-dimensional table of `doubles`.
+    class DataFrame final {
+    public:
+        /// Constructs an empty `DataFrame`.
+        DataFrame() = default;
+
+        /// Constructs a `DataFrame` with the given column names and data.
+        ///
+        /// Throws an exception if `column_names.size() != column_data.size()`.
+        explicit DataFrame(
+            std::vector<std::string> column_names,
+            std::vector<std::vector<double>> column_data,
+            std::unordered_map<std::string, std::string> attrs = {}
+        );
+
+        /// Returns this `DataFrame`'s column labels.
+        std::vector<std::string> columns() const;
+
+        /// Returns this `DataFram`'s metadata (e.g. header key-values).
+        std::unordered_map<std::string, std::string> attrs() const;
+    private:
+        std::vector<std::string> column_names_;
+        std::vector<std::vector<double>> column_data_;
+        std::unordered_map<std::string, size_t> column_to_index_lookup_;
+        std::unordered_map<std::string, std::string> attrs_;
+    };
+}
