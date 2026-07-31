0cde7092f5a6241ada5659c59485bfbebe8e2f88
Adam Kewley <contact@adamkewley.com>
2026-07-31 17:02:45 +0200

    Add initializers to RenderPassConfig fields.

--- liboscar/graphics/render_pass_config.h
+++ liboscar/graphics/render_pass_config.h
@@ -21,7 +21,7 @@ namespace osc
         ///
         /// `std::nullopt` makes the renderer use the full extents of the
         /// render target.
-        std::optional<Rect> viewport_rect;
+        std::optional<Rect> viewport_rect{};
 
         /// The scissor rectangle, which tells the renderer to only clear and/or
         /// render fragments (pixels) that occur within the given sub-rectangle in the
@@ -34,7 +34,7 @@ namespace osc
         /// `std::nullopt` implies that the camera should clear (if applicable) the entire
         /// output, followed by writing output fragments to the full extents of `pixel_rect`
         /// with no scissoring.
-        std::optional<Rect> scissor_rect;
+        std::optional<Rect> scissor_rect{};
 
         /// The color that the render target's color attachments should be cleared
         /// with - if `clear_flags` indicates the attachments should be cleared.
